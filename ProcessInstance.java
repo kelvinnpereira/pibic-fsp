35b10cb4cdc642cb03f11c4c75048c1c29fed0b2
kelvinnpereira <kdsnp@icomp.ufam.edu.br>
2019-05-25 18:09:04 -0400

    init

---
+++ ProcessInstance.java
@@ -0,0 +1,11 @@
+class ProcessInstance{
+
+    private ArrayList<Processo> processos;
+    private ArrayList<ProcessoLocal> locais;
+    private ArrayList<Acao> traceArray;
+    private ArrayList<Range> rangeArray;
+    private ArrayList<Const> constArray;
+    private HiperGrafo grafo;
+    
+
+}
