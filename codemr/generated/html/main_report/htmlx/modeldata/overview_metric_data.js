eb318738df57c45e9f1bb5c9efaba19338a9241b
kelvinnpereira <kdsnp@icomp.ufam.edu.br>
2019-07-29 20:44:30 -0400

    rm codemr

--- codemr/generated/html/main_report/htmlx/modeldata/overview_metric_data.js
+++
@@ -1,1 +0,0 @@
-var CodeMR_ProjectMetrics = {"Project Name":"pibic", "Number of Highly Problematic Classes":0, "Number of Entities":36, "Number of Problematic Classes":0, "Class Lines of Code":877, "Number of External Packages":0, "Number of Packages":1, "Number of External Entities":0}
